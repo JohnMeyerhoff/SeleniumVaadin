72e2075b7c8f5a7e825c5aa83ea4c40aaaee0ec1
John <john@meyerhoff.org>
2022-06-21 13:47:54 +0200

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
