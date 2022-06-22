53ee9ed969bea0d0a8248dce7611d1494bc8d353
John <john@meyerhoff.org>
2022-06-22 13:33:09 +0200

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -32,14 +34,26 @@ public class LoginTest {
     driver.findElement(By.xpath("//*[@id=\"vaadinLoginUsername\"]/input")).sendKeys("AdvTest");
     // find password by xpath AND navigate to BUTTON
     driver.findElement(By.xpath("//*[@id=\"vaadinLoginPassword\"]/input")).sendKeys("AdvTest", Keys.TAB, Keys.SPACE);
-    String error = driver.findElement(By.xpath("//*[@id=\"overlay\"]/flow-component-renderer/div")).getText();
-    assertEquals("User not found.", error);
-    driver.findElement(By.xpath("//*[@id=\"vaadinLoginPassword\"]/input")).sendKeys("AdvTest", Keys.TAB, Keys.SPACE,Keys.ESCAPE);
+    WebElement error = driver.findElement(By.xpath("//*[@id=\"overlay\"]/flow-component-renderer/div"));
+    assertEquals("User not found.", error.getText(), "Message should be 'User not found.'");
 
-    /*new WebDriverWait(driver, 5000).until(
+
+    driver.get("http://localhost:8080/login");
+
+    // find username by xpath
+    driver.findElement(By.xpath("//*[@id=\"vaadinLoginUsername\"]/input")).sendKeys("LauraStudent");
+    // find password by xpath AND navigate to BUTTON
+    driver.findElement(By.xpath("//*[@id=\"vaadinLoginPassword\"]/input")).sendKeys("LauraStudent", Keys.TAB, Keys.SPACE);
+    //assertEquals(driver.getCurrentUrl(), "http://localhost:8080/welcome", "Should be redirected to homepage.");
+
+    WebElement abmelden = driver.findElement(By.partialLinkText("Abmelden"));
+    assertEquals("Abmelden", abmelden.getText(), "Should be 'Abmelden'");
+
+
+    new WebDriverWait(driver, 5000).until(
         ExpectedConditions.presenceOfElementLocated(By.id("existiertbestimmtnicht")));
     driver.get("http://localhost:8080/login");
-*/
+
 
   }
 
